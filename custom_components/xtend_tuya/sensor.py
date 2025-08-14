5f673d92cf5963bf3793da368bfd29c236e133fc
Jurgen De León Rodriguez <judero01@gmail.com>
2025-08-14 14:27:43 -0500

    ok

--- custom_components/xtend_tuya/sensor.py
+++ custom_components/xtend_tuya/sensor.py
@@ -882,7 +882,27 @@ SENSORS: dict[str, tuple[XTSensorEntityDescription, ...]] = {
             entity_registry_visible_default=False,
             restoredata=True,
             refresh_device_after_load=True,
-        )        
+        ),
+         XTSensorEntityDescription(
+            key=XTDPCode.DEVICE_STATE_1,
+            translation_key="device_state1",                        
+            entity_registry_enabled_default=True,
+        ),
+            XTSensorEntityDescription(
+            key=XTDPCode.DEVICE_STATE_2,
+            translation_key="device_state2",          
+            entity_registry_enabled_default=True,
+        ),
+            XTSensorEntityDescription(
+            key=XTDPCode.POWER_TYPE_1,
+            translation_key="power_type1",            
+            entity_registry_enabled_default=True,
+        ),
+            XTSensorEntityDescription(
+            key=XTDPCode.POWER_TYPE_2,
+            translation_key="power_type2",            
+            entity_registry_enabled_default=True,
+        ),
     ),
     "cl": (*BATTERY_SENSORS,),
     "dbl": (
@@ -1367,31 +1387,7 @@ SENSORS: dict[str, tuple[XTSensorEntityDescription, ...]] = {
         *TEMPERATURE_SENSORS,
         *HUMIDITY_SENSORS,
         *BATTERY_SENSORS,
-    ),
-    "cz" : (
-        *CONSUMPTION_SENSORS,        
-        *ELECTRICITY_SENSORS,
-        XTSensorEntityDescription(
-            key=XTDPCode.DEVICE_STATE_1,
-            translation_key="device_state1",                        
-            entity_registry_enabled_default=True,
-        ),
-            XTSensorEntityDescription(
-            key=XTDPCode.DEVICE_STATE_2,
-            translation_key="device_state2",          
-            entity_registry_enabled_default=True,
-        ),
-            XTSensorEntityDescription(
-            key=XTDPCode.POWER_TYPE_1,
-            translation_key="power_type1",            
-            entity_registry_enabled_default=True,
-        ),
-            XTSensorEntityDescription(
-            key=XTDPCode.POWER_TYPE_2,
-            translation_key="power_type2",            
-            entity_registry_enabled_default=True,
-        ),
-    )
+    )    
 }
 
 # Socket (duplicate of `kg`)
